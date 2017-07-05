d92370018be17bcd381b58f739e52f4bf46cff96
Mathu Mounasamy <mmounasamy@gmail.com>
2017-07-06 00:26:15 +1000

    Create the database context

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -9,5 +9,15 @@ namespace TodoApi.Models
         public bool IsComplete { get; set; }
     }
 
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
     
 }
